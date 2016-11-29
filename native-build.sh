0483ca2473a2cb811a9f1a13d1cf1c2dd5739bdc
Albert Casals <skarbat@gmail.com>
2016-11-29 20:51:09 +0000

    Script to build QT5 natively to get the toolchain

--- native-build.sh
+++ native-build.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 #
-# Script to build QT5 core tools natively
-# It is executed by "packageme" script
+# Script to build QT5 natively on the sysroot.
 #
+# This allows to build QT5 apps on the RaspberryPI
+#
+
+qt5_install=$1
+
+if [ "$qt5_install" == "" ]; then
+    echo "Syntax error: native-build.sh <qt5 install directory>"
+    exit 1
+fi
 
 if [ `uname -m` != "armv7l" ]; then
     echo "Not running in emulated ARM mode - aborting"
     exit 1
 fi
 
-cd /tmp/qt5-native/qtbase
+echo ">>> Configure..."
+cd /tmp/qt5
 ./configure -device linux-rasp-pi2-g++ \
 	    -device-option CROSS_COMPILE=/usr/bin/ \
 	    -opensource -confirm-license -release \
-	    -prefix /usr/local/qt5 -pkg-config -no-pch \
+	    -prefix $qt5_install -pkg-config -no-pch \
+	    -skip qtwebengine -skip qtwebview -skip qtwayland \
 	    -no-use-gold-linker -xkb-config-root /usr/share/X11/xkb \
 	    -nomake tests -nomake examples -verbose -no-warnings-are-errors \
 	    -no-qml-debug -optimized-qmake -strip -fontconfig -no-sql-sqlite
 
+exit 0
 
+echo ">> Make..."
+make
+
+echo ">> Make Install..."
+make install
